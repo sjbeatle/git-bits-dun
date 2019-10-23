ce3f3d0cfa766b090f317b485b47b7affd2ff5f2
Steve Black <stevenjblack@gmail.com>
2019-10-23 14:03:20 -0400

    Integrated FontAwesome: `npm i` required

--- packages/app/src/app/app.module.ts
+++ packages/app/src/app/app.module.ts
@@ -2,15 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FontAwesomeModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
 export class AppModule { }
