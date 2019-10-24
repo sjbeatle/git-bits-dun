91b7a0790d3a8af21ca0b0faf1a3fe5e7c26cf32
Steve Black <stevenjblack@gmail.com>
2019-10-24 06:39:29 -0400

    `git-bits-dun` component initialized

---
+++ packages/app/src/app/components/git-bits-dun/git-bits-dun.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-git-bits-dun',
+  templateUrl: './git-bits-dun.component.html',
+  styleUrls: ['./git-bits-dun.component.scss']
+})
+export class GitBitsDunComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
