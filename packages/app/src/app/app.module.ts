b7ab9d35ed1dda61bd9e5aaca4fa65935a5b46ab
Steven Black <sblack@us.ibm.com>
2019-10-23 22:00:17 -0400

    `messages` component initialization

--- packages/app/src/app/app.module.ts
+++ packages/app/src/app/app.module.ts
@@ -6,12 +6,14 @@ import { AppComponent } from './app.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { MastheadComponent } from './components/masthead/masthead.component';
 import { LogoComponent } from './components/logo/logo.component';
+import { MessagesComponent } from './components/messages/messages.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MastheadComponent,
     LogoComponent,
+    MessagesComponent,
   ],
   imports: [
     BrowserModule,
