2d54c6f69ffcdbcc68aca6b0f95f6b6f6041afab
Steve Black <stevenjblack@gmail.com>
2019-10-24 04:14:13 -0400

    `bit` component refactored and integrated

--- packages/app/src/app/components/bit/bit.component.ts
+++ packages/app/src/app/components/bit/bit.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { ITodo } from 'src/app/services/types';
+import { faCircle as faCircleO } from '@fortawesome/free-regular-svg-icons';
+import { faCircle, faCalendarDay } from '@fortawesome/free-solid-svg-icons';
+
+import { TodoService } from '../../services';
 
 @Component({
   selector: 'app-bit',
@@ -6,8 +11,40 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./bit.component.scss']
 })
 export class BitComponent implements OnInit {
+  @Input() todo: ITodo;
+  isDeleting = false;
+  faCircle = faCircle;
+  faCircleO = faCircleO;
+  faCalendarDay = faCalendarDay;
 
-  constructor() { }
+  constructor(private todoService: TodoService) { }
+
+  deleteBit() {
+    this.isDeleting = true;
+    this.todoService.deleteTodo(this.todoId)
+      .subscribe(() => {
+        this.isDeleting = false;
+      });
+  }
+
+  get todoId() {
+    return this.todo._id;
+  }
+
+  get label() {
+    return this.todo.todo;
+  }
+
+  get date() {
+    return new Date(this.todo.createdDate).toLocaleDateString(undefined, {
+      month: 'short',
+      day: 'numeric',
+    });
+  }
+
+  get circle() {
+    return this.isDeleting ? this.faCircle : this.faCircleO;
+  }
 
   ngOnInit() {
   }
